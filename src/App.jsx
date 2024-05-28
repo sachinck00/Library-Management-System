6c3d85f7829e5fe199f632f137f06a9d3e09c0c6
sachinck00 <sachinckadlagond3747@gmail.com>
2024-05-28 16:24:21 +0530

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from 'react'
 import './App.css'
-
+import Search from './components/Search'
 function App() {
   return (
     <>
-      
+      <Search />
     </>
-  )
+  );
 }
 
 export default App
